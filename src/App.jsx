6e3f798131e6a24b5497f1ab3c8a999e1d1e0555
daniel-rogerio <danielr.xp@gmail.com>
2024-03-29 23:47:11 -0300

    feature: exibir o componente CardTrilha
    Esse commit é referente ao exercício:
    [M1S10] Ex. 5 - Exibindo os cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import './App.css'
 import { CardTrilha } from './components/CardTrilha'
 
 function App() {
+
+  const trailImage = 'https://th.bing.com/th/id/OIP.IcTZZ1-5Gwa76smIvmrYIgHaE8?rs=1&pid=ImgDetMain'
+
   return (
-      <CardTrilha />
+      <CardTrilha 
+        imgURL={trailImage}
+        trailName='Trilha da Costa da Lagoa'
+        city='Florianópolis'
+        state='SC'
+        author='Daniel Rogerio'
+        duration={120}
+        distance={4}
+        difficult='Iniciante'
+      />
   )
 }
 
